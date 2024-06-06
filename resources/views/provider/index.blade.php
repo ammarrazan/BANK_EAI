7aa680c919fbce8411d0517f0e1a29d567413422
Tomen1980 <agungjumantoroandrian@gmail.com>
2024-06-06 20:57:33 +0700

    +fix css dan provider

--- resources/views/provider/index.blade.php
+++ resources/views/provider/index.blade.php
@@ -9,11 +9,11 @@
 </head>
 
 <body class="bg-sky-300">
-    <div class="w-full md:w-1/2 m-auto border-2 border-red-400 mt-5 py-5 px-4 md:px-0">
+    <div class="w-full md:w-3/4 m-auto border-2 border-red-400 mt-5 py-5 px-4 md:px-0">
         <h1 class="text-3xl md:text-5xl text-center font-bold text-black">Dokumentasi Bank EAI</h1>
 
-        <div class="w-full mt-10 space-y-5">
-            <div class="w-full md:w-3/4 m-auto bg-white border-gray-500 h-auto md:h-40 rounded-lg shadow-lg shadow-gray-400 text-center p-4 md:p-0">
+        <div class="w-full mt-10 grid grid-cols-1 md:grid-cols-2 gap-5">
+            <div class="bg-white border-gray-500 h-auto rounded-lg shadow-lg shadow-gray-400 text-center p-4">
                 <h3 class="text-xl font-bold">Data Tabungan</h3>
                 <hr class="border-1 border-gray-500 my-2">
                 <h3 class="text-lg font-semibold mt-3">List Jenis Data Tabungan</h3>
@@ -22,7 +22,7 @@
                 <p class="text-sm">API : GET /tabungan/{id}</p>
             </div>
 
-            <div class="w-full md:w-3/4 m-auto bg-white border-gray-500 h-auto md:h-40 rounded-lg shadow-lg shadow-gray-400 text-center p-4 md:p-0">
+            <div class="bg-white border-gray-500 h-auto rounded-lg shadow-lg shadow-gray-400 text-center p-4">
                 <h3 class="text-xl font-bold">Jenis Data Nasabah</h3>
                 <hr class="border-1 border-gray-500 my-2">
                 <h3 class="text-lg font-semibold mt-3">List Jenis Data Nasabah</h3>
@@ -31,7 +31,7 @@
                 <p class="text-sm">API : GET /nasabah/{id}</p>
             </div>
 
-            <div class="w-full md:w-3/4 m-auto bg-white border-gray-500 h-auto md:h-40 rounded-lg shadow-lg shadow-gray-400 text-center p-4 md:p-0">
+            <div class="bg-white border-gray-500 h-auto rounded-lg shadow-lg shadow-gray-400 text-center p-4">
                 <h3 class="text-xl font-bold">Data Pembayaran</h3>
                 <hr class="border-1 border-gray-500 my-2">
                 <h3 class="text-lg font-semibold mt-3">List Jenis Data Pembayaran</h3>
@@ -40,7 +40,7 @@
                 <p class="text-sm">API : GET /pembayaran/{id}</p>
             </div>
 
-            <div class="w-full md:w-3/4 m-auto bg-white border-gray-500 h-auto md:h-40 rounded-lg shadow-lg shadow-gray-400 text-center p-4 md:p-0">
+            <div class="bg-white border-gray-500 h-auto rounded-lg shadow-lg shadow-gray-400 text-center p-4">
                 <h3 class="text-xl font-bold">Kartu Kredit</h3>
                 <hr class="border-1 border-gray-500 my-2">
                 <h3 class="text-lg font-semibold mt-3">List Jenis Data Kartu Kredit</h3>
@@ -48,53 +48,47 @@
                 <h3 class="text-lg font-semibold mt-3">Detail Data Kartu Kredit</h3>
                 <p class="text-sm">API : GET /kartu-kredit/{id}</p>
             </div>
-          
-            <div class="w-full md:w-3/4 m-auto bg-white border-gray-500   rounded-lg shadow-lg shadow-gray-400 text-center p-4 md:p-0">
+
+            <div class="bg-white border-gray-500 h-auto rounded-lg shadow-lg shadow-gray-400 text-center p-4">
                 <h3 class="text-xl font-bold">OBJECT WISATA</h3>
                 <hr class="border-1 border-gray-500 my-2">
-            <div class="space-y-3">
-                <div>
-                    <h3 class="text-lg font-semibold ">Tampil Seluruh Akun Bank</h3>
-                    <p class="text-sm">API : GET /api/obj-wisata</p>
-                </div>
-            <div>
-                <h3 class="text-lg font-semibold ">Tampil Detail Akun Bank</h3>
-                <p class="text-sm">API : GET /api/obj-wisata</p>
-            </div>
-               
-                
-                <div>
-                    <h3 class="text-lg font-semibold ">Buat Akun Bank</h3>
-                    <p class="text-sm">API : POST /api/obj-wisata</p>
-                    <ul>
-                        <li>DATA Yang <span class="font-bold">dibutuhkan :</span></li>
-                        <li>id_wisata ex : "OP01"</li>
-                        <li>nama_objek ex : "North Point"</li>
-                        <li>harga_tiket : "100000"</li>
-                        <li>saldo : "100000"</li>
-                    </ul>
-                </div>
-                
-                <div>
-                    <h3 class="text-lg font-semibold ">Update Akun Bank</h3>
-                    <p class="text-sm">API : Update /api/obj-wisata/{rekening(id)}</p>
-                    <ul>
-                        <li>DATA Yang <span class="font-bold">dibutuhkan :</span></li>
-                        <li>id_wisata ex : "OP01"</li>
-                        <li>nama_objek ex : "North Point"</li>
-                        <li>harga_tiket : "100000"</li>
-                        <li>saldo : "100000"</li>
-                    </ul>
-                </div>
-              
-                <div>
-                    <h3 class="text-lg font-semibold ">Hapus Akun Bank</h3>
-                    <p class="text-sm">API : Update /api/obj-wisata/{rekening(id)}</p>
+                <div class="space-y-3">
+                    <div>
+                        <h3 class="text-lg font-semibold">Tampil Seluruh Akun Bank</h3>
+                        <p class="text-sm">API : GET /api/obj-wisata</p>
+                    </div>
+                    <div>
+                        <h3 class="text-lg font-semibold">Tampil Detail Akun Bank</h3>
+                        <p class="text-sm">API : GET /api/obj-wisata</p>
+                    </div>
+                    <div>
+                        <h3 class="text-lg font-semibold">Buat Akun Bank</h3>
+                        <p class="text-sm">API : POST /api/obj-wisata</p>
+                        <ul>
+                            <li>DATA Yang <span class="font-bold">dibutuhkan :</span></li>
+                            <li>id_wisata ex : "OP01"</li>
+                            <li>nama_objek ex : "North Point"</li>
+                            <li>harga_tiket : "100000"</li>
+                            <li>saldo : "100000"</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 class="text-lg font-semibold">Update Akun Bank</h3>
+                        <p class="text-sm">API : Update /api/obj-wisata/{rekening(id)}</p>
+                        <ul>
+                            <li>DATA Yang <span class="font-bold">dibutuhkan :</span></li>
+                            <li>id_wisata ex : "OP01"</li>
+                            <li>nama_objek ex : "North Point"</li>
+                            <li>harga_tiket : "100000"</li>
+                            <li>saldo : "100000"</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 class="text-lg font-semibold">Hapus Akun Bank</h3>
+                        <p class="text-sm">API : Update /api/obj-wisata/{rekening(id)}</p>
+                    </div>
                 </div>
             </div>
-               
-            </div>
-
         </div>
     </div>
 </body>
--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ObjWisataController;
 
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('/obj-wisata', ObjWisataController::class);
-// Route::post('/obj-wisata', [ObjWisataController::class, 'store']);
-
-Route::post("/obj-wisata", [ObjWisataController::class, "store"]);
